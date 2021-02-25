6f7dd6692cc2f43f7b7c2b4e37a50ab00592def6
MoldsonBrown <42676431+MoldsonBrown@users.noreply.github.com>
2021-02-25 18:24:11 -0300

    verificando el php

--- nacho/assets/php/send.php
+++ nacho/assets/php/send.php
@@ -1,9 +1,9 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Smith" );
-define( "RECIPIENT_EMAIL", "booharry@gmail.com" );
-define( "EMAIL_SUBJECT", "Visitor Message" );
+define( "RECIPIENT_NAME", "Estudio Llaver" );
+define( "RECIPIENT_EMAIL", "info@estudiollaver.com" );
+define( "EMAIL_SUBJECT", "Consulta Web" );
 
 // Read the form values
 $success = false;
@@ -11,14 +11,8 @@ $senderName = isset( $_POST['senderName'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9
 $senderEmail = isset( $_POST['senderEmail'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['senderEmail'] ) : "";
 $message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
 
-$humanA     = $_POST['checkHuman_a'];
-$humanB     = $_POST['checkHuman_b'];
-$humanCheck = $_POST['senderHuman'];
-
-$human = ($humanCheck == $humanA + $humanB) ? true : false;
 
 // If all values exist, send the email
-if( $human == true ) {
     if ( $senderName && $senderEmail && $message) {
       $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
       $headers = "From: " . $senderName . " <" . $senderEmail . ">";
@@ -33,16 +27,13 @@ if ( isset($_GET["ajax"]) ) {
 ?>
 <html>
   <head>
-    <title>Thanks!</title>
+    <title>Gracias!!</title>
   </head>
   <body>
-  <?php if ( $success ) echo "<p>Thanks for sending your message! We'll get back to you shortly.</p>" ?>
-  <?php if ( !$success ) echo "<p>There was a problem sending your message. Please try again.</p>" ?>
-  <p>Click your browser's Back button to return to the page.</p>
+  <?php if ( $success ) echo "<p>Enviamos su consulta! Nos contactaremos a la brevedad.</p>" ?>
+  <?php if ( !$success ) echo "<p>Hubo un problema al enviar el mensaje. Por favor intente nuevamente.</p>" ?>
   </body>
 </html>
 <?php
 }
 ?>
-
-
